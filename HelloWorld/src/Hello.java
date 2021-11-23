6cd7786c478bd343b6997f987965ac597d92f338
Sundar Raj <sundar.raj@gurucul.com>
2021-11-23 14:41:47 +0530

    Varibale int

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Sundar");
+        int myFirstNumber = (10+5)+(2*10);
+        System.out.println(myFirstNumber);
     }
 }
