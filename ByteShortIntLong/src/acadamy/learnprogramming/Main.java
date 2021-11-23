ddec3dc853be99f91beb78354e39c613917dc8f0
Sundar Raj <sundar.raj@gurucul.com>
2021-11-23 16:16:02 +0530

    Casting in Java

--- ByteShortIntLong/src/acadamy/learnprogramming/Main.java
+++ ByteShortIntLong/src/acadamy/learnprogramming/Main.java
@@ -5,10 +5,10 @@ public class Main {
     public static void main(String[] args) {
 	int myMinIntValue=Integer.MIN_VALUE;
 	int myMaxIntValue=Integer.MAX_VALUE;
-        System.out.println("Minimum Value "+ myMinIntValue);
-        System.out.println("Maximum Value "+ myMaxIntValue);
-        System.out.println("Busted Maximum Value "+ (myMaxIntValue+1));
-        System.out.println(" Busted Minimum Value "+ (myMinIntValue-1));
+        System.out.println("Minimum int Value "+ myMinIntValue);
+        System.out.println("Maximum int Value "+ myMaxIntValue);
+        System.out.println("Busted Maximum int Value "+ (myMaxIntValue+1));
+        System.out.println(" Busted Minimum int Value "+ (myMinIntValue-1));
     int myMaxIntTest=2_147_483_647;
         System.out.println(myMaxIntTest);
 
@@ -29,5 +29,15 @@ public class Main {
 
         long bigLongValue=2_147_483_647_787L;
         System.out.println("my bigLongValue "+ bigLongValue);
+
+        int myTotal=(myMinIntValue/2);
+        System.out.println(myTotal);
+
+        byte newByteValue=(byte) (myMinByteValue/2);
+        System.out.println(newByteValue);
+
+        short newShortValue=(short) (myMinShortValue/2);
+        System.out.println(newShortValue);
+
     }
 }
