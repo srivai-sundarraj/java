aad1e3911b17de2920792c15868a690e38eb77a8
Sundar Raj <sundar.raj@gurucul.com>
2021-11-23 17:42:53 +0530

    Floating Point Precision and a Challenge

--- FloatAndDouble/src/acadamy/learnprogramming/Main.java
+++ FloatAndDouble/src/acadamy/learnprogramming/Main.java
@@ -16,7 +16,21 @@ public class Main {
     int myIntValue=5;
     //float myFloatValue=5.25;
     //float myFloatValue=(float)5.25;
-    float myFloatValue=5.25f;
-    double myDoubleValue=5.25;
+    float myFloatValue=5f/3f;
+    //double myDoubleValue=5d/3d;
+    double myDoubleValue=5.00/3.00;
+        System.out.println("Int Value : "+myIntValue);
+        System.out.println("Float Value : "+myFloatValue);
+        System.out.println("Double Value : "+myDoubleValue);
+
+    double numberOfPounds=200d;
+    double convertedKilogram=numberOfPounds*0.45359237d;
+        System.out.println("Kilogram : "+ convertedKilogram);
+
+    double pi=3.1415927;
+    double anotherNumber=5_000_000.787890d;
+        System.out.println(pi);
+        System.out.println(anotherNumber);
+
     }
 }
