6570b23eaf6cdb107110ea4ec013342b3fe734d6
Sundar Raj <sundar.raj@gurucul.com>
2021-11-23 18:10:15 +0530

    The char and boolean Primitive Data Types

---
+++ CharAndBoolean/src/acadamy/learnprogramming/Main.java
@@ -0,0 +1,19 @@
+package acadamy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	char charValue='D';
+	char unicodeValue='\u0044';
+        System.out.println(charValue);
+        System.out.println(unicodeValue);
+    char copyRight='\u00A9';
+        System.out.println(copyRight);
+
+    boolean trueBooleanValue=true;
+    boolean falseBooleanValue=false;
+    boolean isCustomerOverTwentyOne=true;
+
+
+    }
+}
