32a2d8284807cf7e47aad8e48d6d13ffd2b7d281
Sundar Raj <sundar.raj@gurucul.com>
2021-11-23 15:48:43 +0530

    byte, short, long and width

--- ByteShortIntLong/src/acadamy/learnprogramming/Main.java
+++ ByteShortIntLong/src/acadamy/learnprogramming/Main.java
@@ -11,5 +11,23 @@ public class Main {
         System.out.println(" Busted Minimum Value "+ (myMinIntValue-1));
     int myMaxIntTest=2_147_483_647;
         System.out.println(myMaxIntTest);
+
+        byte myMinByteValue=Byte.MIN_VALUE;
+        byte myMaxByteValue=Byte.MAX_VALUE;
+        System.out.println("Minimum Byte Value : "+myMinByteValue);
+        System.out.println("Maximum Byte Value : "+myMaxByteValue);
+
+        short myMinShortValue=Short.MIN_VALUE;
+        short myMaxShortValue=Short.MAX_VALUE;
+        System.out.println("Minimum Short Value : "+myMinShortValue);
+        System.out.println("Maximum Short Value : "+myMaxShortValue);
+
+        long myMinLongValue=Long.MIN_VALUE;
+        long myMaxLongValue=Long.MAX_VALUE;
+        System.out.println("Minimum Long Value : "+myMinLongValue);
+        System.out.println("Maximum Long Value : "+myMaxLongValue);
+
+        long bigLongValue=2_147_483_647_787L;
+        System.out.println("my bigLongValue "+ bigLongValue);
     }
 }
