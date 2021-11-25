2e34e1bdcc7edb8f228fca9a7007eaafc6af1e4e
Sundar Raj <sundar.raj@gurucul.com>
2021-11-25 11:02:13 +0530

    Statements, Whitespace and Indentation (Code Organization)

---
+++ 2_JavaTutorialExpressionsStatementsCodeblocksMethodsandmore/StatementsWhiteSpaceAndIndenting/src/acadamy/learnprogramming/Main.java
@@ -0,0 +1,15 @@
+package acadamy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myVariable = 30;
+        myVariable++;
+        myVariable--;
+        if (myVariable == 30) {
+            System.out.println("Value " +
+                    "is correct : " +
+                    myVariable);
+        }
+    }
+}
